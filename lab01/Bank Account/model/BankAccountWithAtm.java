38ff67071e135450a18b56fc618b5caad678aa39
antonioIannotta <74740099+antonioIannotta@users.noreply.github.com>
2022-03-02 11:01:25 +0100

    Add files via upload

---
+++ lab01/Bank Account/model/BankAccountWithAtm.java
@@ -0,0 +1,46 @@
+package lab01.example.model;
+
+public class BankAccountWithAtm implements BankAccount{
+
+    private final AccountHolder holder;
+    private double balance;
+    private final int FEE = 1;
+
+
+    public BankAccountWithAtm(AccountHolder holder, double balance) {
+        this.balance = balance;
+        this.holder = holder;
+    }
+
+    @Override
+    public AccountHolder getHolder() {
+        return this.holder;
+    }
+
+    @Override
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public void deposit(int userID, double amount) {
+        if (checkUser(userID)){
+            balance += (amount - FEE);
+        }
+    }
+
+    @Override
+    public void withdraw(int userID, double amount) {
+        if (checkUser(userID) && isWithdrawAllowed(amount)){
+            this.balance -= (amount + FEE);
+        }
+    }
+
+    private boolean checkUser(final int id){
+        return this.holder.getId() == id;
+    }
+
+    private boolean isWithdrawAllowed(final double amount){
+        return (this.getBalance() >= (amount + FEE));
+    }
+}
